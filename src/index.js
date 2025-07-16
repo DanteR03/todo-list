330616ebb2f40d426b0fe0bd9a6f8222dc418818
Lev <101640837+DanteR03@users.noreply.github.com.>
2025-07-17 02:21:43 +0500

    Create a class for projects in JS

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import Task from "./task.js";
+import Project from "./projects.js";
+
+let defaultProject = new Project("Default");
 
 let task1 = new Task("exampleOne", "exampleDescOne", "01.01.1111", "highest priority");
 let task2 = new Task("exampleTwo", "exampleDescTwo", "10.01.1111", "medium priority");
 let task3 = new Task("exampleThree", "exampleDescThree", "01.05.1111", "low priority");
 
-console.log(task1, task2, task3);
+defaultProject.AddTask(task1);
+defaultProject.AddTask(task2);
+defaultProject.AddTask(task3);
+
+console.log(defaultProject.title);
