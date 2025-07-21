dafab8adfc772217a9e6754b980eb505d5f2eb23
Lev <101640837+DanteR03@users.noreply.github.com.>
2025-07-21 05:11:07 +0500

    Add methods to find and delete tasks from projects

--- src/index.js
+++ src/index.js
@@ -7,8 +7,7 @@ let task1 = new Task("exampleOne", "exampleDescOne", "01.01.1111", "highest prio
 let task2 = new Task("exampleTwo", "exampleDescTwo", "10.01.1111", "medium priority");
 let task3 = new Task("exampleThree", "exampleDescThree", "01.05.1111", "low priority");
 
-defaultProject.AddTask(task1);
-defaultProject.AddTask(task2);
-defaultProject.AddTask(task3);
+defaultProject.addTask(task1);
+defaultProject.addTask(task2);
+defaultProject.addTask(task3);
 
-console.log(task1.getTaskInfo("title"));
--- src/projects.js
+++ src/projects.js
@@ -4,7 +4,25 @@ export default class Project {
         this.tasks = [];
     }
 
-    AddTask(task) {
+    addTask(task) {
         this.tasks.push(task);
+    }
+
+    findTask(key, value) {
+        let foundTask = "";
+        this.tasks.forEach((task) => {
+            if (task[key] === value) {
+                foundTask = task;
+            }
+        });
+        return foundTask;
+    }
+
+    deleteTask(key, value) {
+        let deletedTask = this.findTask(key, value);
+        let deletedTaskIndex = this.tasks.indexOf(deletedTask);    
+        if (deletedTaskIndex !== -1) {
+            this.tasks.splice(deletedTaskIndex, 1);
+        }
     }
 }
